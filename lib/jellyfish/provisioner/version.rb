614f63344c7218edeaa8eccea5b6a0ecc11d21e7
Caleb Thompson <caleb@calebthompson.io>
2015-04-08 14:54:49 -0500

    Add Provisioner and specs

--- lib/jellyfish/provisioner/version.rb
+++ lib/jellyfish/provisioner/version.rb
@@ -1,5 +1,5 @@
 module Jellyfish
-  module Provisioner
+  class Provisioner
     VERSION = "0.1.0"
   end
 end
--- spec/jellyfish/provisioner_spec.rb
+++ spec/jellyfish/provisioner_spec.rb
@@ -1,11 +1,66 @@
 require 'spec_helper'
 
 describe Jellyfish::Provisioner do
-  it 'has a version number' do
-    expect(Jellyfish::Provisioner::VERSION).not_to be nil
+  before(:each) { class_double("OrderItem", find: order_item).as_stubbed_const }
+
+  it 'finds an order item and initailizes with it, then saves the record' do
+    allow(order_item).to receive(:save!)
+
+    provisioner.provision(1)
+
+    expect(OrderItem).to have_received(:find)
+    expect(order_item).to have_received(:save!)
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '.provision' do
+    it 'delegates to instance' do
+      expect(provisioner.provision(1)).to eq 'provisioned'
+    end
+
+    it 'notes a critical status and stores a message' do
+      allow_any_instance_of(provisioner).to receive(:provision).and_raise('strong bad')
+
+      expect { provisioner.provision(1) }.to raise_error
+
+      expect(order_item.provision_status).to eq :critical
+      expect(order_item.status_msg).to eq('strong bad')
+    end
+  end
+
+  describe '.retire' do
+    it 'delegates to instance' do
+      expect(provisioner.retire(1)).to eq 'retired'
+    end
+
+    it 'notes a warning status and stores an error' do
+      allow_any_instance_of(provisioner).to receive(:retire).and_raise('weak bad')
+
+      expect { provisioner.retire(1) }.to raise_error
+
+      expect(order_item.provision_status).to eq :warning
+      expect(order_item.status_msg).to eq('Retirement failed: weak bad')
+    end
+  end
+
+  def provisioner
+    @provisioner ||= Class.new(Jellyfish::Provisioner) do
+      def provision
+        'provisioned'
+      end
+
+      def retire
+        'retired'
+      end
+    end
+  end
+
+  def order_item
+    @order_item ||= Class.new do
+      attr_accessor :provision_status, :status_msg
+
+      def save!
+        'saved'
+      end
+    end.new
   end
 end
